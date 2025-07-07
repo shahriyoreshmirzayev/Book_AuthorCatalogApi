6f29057e185f240fc613f619796a53f85786f719
Shahriyor <139459691+shahriyoreshmirzayev@users.noreply.github.com>
2025-07-07 17:50:43 +0500

    GetAllBooks va GetAllAuthors qo'shildi

--- BookCatalogApi/Program.cs
+++ BookCatalogApi/Program.cs
@@ -9,44 +9,30 @@ namespace BookCatalogApi
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-
             builder.Services.AddInfrastructureServices(builder.Configuration);
             builder.Services.AddApplicationServices();
-
             builder.Services.AddMemoryCache(); 
-            
             builder.Services.AddStackExchangeRedisCache(setupAction =>
             {
                 setupAction.Configuration = builder.Configuration.GetConnectionString("RedisConnectionString");
             });
-
             builder.Services.AddResponseCaching();
-
             builder.Services.AddOutputCache();
-
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
             var app = builder.Build();
-
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-            
             app.UseResponseCaching();
-
             app.UseOutputCache();
             app.UseETagger();
             app.MapControllers();
-
             app.Run();
         }
     }
