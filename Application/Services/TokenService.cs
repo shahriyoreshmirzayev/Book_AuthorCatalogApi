5291658f12e48ee890c5d96b8df09f1698f9cb65
Shahriyor <139459691+shahriyoreshmirzayev@users.noreply.github.com>
2025-07-07 23:28:19 +0500

    appSettingLSON'ga qo'shildi https va http'lar

--- Application/Services/TokenService.cs
+++ Application/Services/TokenService.cs
@@ -5,32 +5,30 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
-namespace Application.Services
-{
-    public class TokenService : ITokenService
-    {
-        private readonly IConfiguration _configuration;
-        public TokenService(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-        public string CreateToken()
-        {
-            var claims = new Claim[]
-            {
-                new (ClaimTypes.Email, "SampleEmail@gmail.com")
-            };
-            var token = new JwtSecurityToken(
-                issuer: _configuration["JWT:IssuerKey"],
-                audience: _configuration["JWT:AudienceKey"],
-                claims: claims,
-                expires: DateTime.UtcNow.AddMinutes(Convert.ToInt32(_configuration["JWT:AccesTokenLifetime"])),
-                signingCredentials: new SigningCredentials(
-                    new SymmetricSecurityKey(
-                        Encoding.UTF8.GetBytes(_configuration["JWT:Key"])),
-                    SecurityAlgorithms.HmacSha256Signature));
+namespace Application.Services;
 
-            return new JwtSecurityTokenHandler().WriteToken(token); 
-        }
+public class TokenService : ITokenService
+{
+    private readonly IConfiguration _configuration;
+    public TokenService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    public string CreateToken()
+    {
+        var claims = new Claim[]
+        {
+            new (ClaimTypes.Email, "SampleEmail@gmail.com")
+        };
+        var token = new JwtSecurityToken(
+            issuer: _configuration["JWT:IssuerKey"],
+            audience: _configuration["JWT:AudienceKey"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(Convert.ToInt32(_configuration["JWT:AccesTokenLifetime"])),
+            signingCredentials: new SigningCredentials(
+                new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(_configuration["JWT:Key"])),
+                SecurityAlgorithms.HmacSha256Signature));
+        return new JwtSecurityTokenHandler().WriteToken(token); 
     }
 }
